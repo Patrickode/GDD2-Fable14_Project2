2cdd25f0ebe4aa65be41d579985e3c02588f4762
Patrick <prm5983@rit.edu>
2020-10-19 18:43:45 -0400

    Made a popup with the level name and description that fades in and out

---
+++ IGME302_Project2/Assets/Scripts/UI/UseLevelInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UseLevelInfo : MonoBehaviour
+{
+    [Tooltip("Where the level info should go. Will be the TMP component of this object, more often that not.")]
+    [SerializeField] private TextMeshProUGUI levelInfoDestination = null;
+    [Tooltip("If this is a header, the level name will be gotten. If it's not, the description will be gotten.")]
+    [SerializeField] private bool isHeader = false;
+
+    private void Awake()
+    {
+        LevelManager.OnLoaded += PopulateWithLevelInfo;
+    }
+    private void OnDestroy()
+    {
+        LevelManager.OnLoaded -= PopulateWithLevelInfo;
+    }
+
+    private void PopulateWithLevelInfo(Level levelInfoSource)
+    {
+        //If the destination is a header, get the level name. Otherwise, get the level description.
+        levelInfoDestination.text = isHeader ? levelInfoSource.levelName : levelInfoSource.description;
+    }
+}
