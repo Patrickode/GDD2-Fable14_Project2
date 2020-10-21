8c4e4e71795dd0226619cd7caefb01f0836c15f6
crlimacastro <crlimacastro@gmail.com>
2020-10-21 14:34:45 -0400

    Implemented Pressure Plates

---
+++ IGME302_Project2/Assets/Scripts/PressurePlates/Effects/ClearObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ClearObstacle : PressurePlateEffect
+{
+    [SerializeField]
+    private List<Vector2Int> colliderPositions;
+
+    private Tilemap Colliders => LevelManager.CurrentLevel.colliders;
+
+    public override void ActivateEffect()
+    {
+        RemoveColliders();
+    }
+
+    private void RemoveColliders()
+    {
+        foreach (Vector2Int position in colliderPositions)
+        {
+            // Set each tile at the position to null to clear them
+            Colliders.SetTile((Vector3Int)position, null);
+        }
+    }
+}
